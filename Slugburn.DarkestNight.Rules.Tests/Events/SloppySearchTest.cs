6f9556e095360f438cc76b52ae85870a10e385ce
Dave Perry <dave@slugburn.com>
2017-06-23 20:39:52 -0700

    Implemented Sloppy Search event

---
+++ Slugburn.DarkestNight.Rules.Tests/Events/SloppySearchTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using Slugburn.DarkestNight.Rules.Tests.Fakes;
+using Slugburn.DarkestNight.Rules.Tests.Fluent;
+
+namespace Slugburn.DarkestNight.Rules.Tests.Events
+{
+    [TestFixture]
+    public class SloppySearchTest
+    {
+        [Test]
+        public void SloppySearch_GainSecrecy()
+        {
+            TestScenario
+                .Given.Game(g => g.Hero(h => h.Secrecy(0)))
+                .When.Hero(h => h.DrawsEvent("Sloppy Search"))
+                .Then.Player(p => p.Event(e => e.HasBody("Sloppy Search", 2, "Roll 1d and take the highest").HasOptions("Roll")))
+                .When.Player(p => p.SelectsEventOption("Roll", x => x.Rolls(6)))
+                .Then.Player(p => p.Event(e => e.ActiveRow("Gain 1 Secrecy").HasOptions("Gain Secrecy")))
+                .When.Player(p => p.SelectsEventOption("Gain Secrecy"))
+                .Then.Hero(h => h.Secrecy(1).Event(e=>e.HasOutstanding(0)));
+        }
+
+        [TestCase(5)]
+        [TestCase(4)]
+        public void SloppySearch_NoEffect(int roll)
+        {
+            TestScenario
+                .Given.Game(g => g.Hero())
+                .When.Hero(h => h.DrawsEvent("Sloppy Search"))
+                .When.Player(p => p.SelectsEventOption("Roll", x=>x.Rolls(roll)))
+                .Then.Player(p => p.Event(e => e.ActiveRow("No effect").HasOptions("No Effect")))
+                .When.Player(p => p.SelectsEventOption("No Effect"))
+                .Then.Hero(h => h.Event(e=>e.HasOutstanding(0)));
+        }
+
+        [TestCase(3)]
+        [TestCase(2)]
+        [TestCase(1)]
+        public void SloppySearch_SpendGrace(int roll)
+        {
+            TestScenario
+                .Given.Game(g => g.Hero())
+                .When.Hero(h => h.DrawsEvent("Sloppy Search"))
+                .When.Player(p => p.SelectsEventOption("Roll", x=>x.Rolls(roll)))
+                .Then.Player(p => p.Event(e => e.ActiveRow("Spend 1 Grace or lose 1 Secrecy").HasOptions("Spend Grace", "Lose Secrecy")))
+                .When.Player(p => p.SelectsEventOption("Spend Grace"))
+                .Then.Hero(h => h.LostGrace().Event(e=>e.HasOutstanding(0)));
+        }
+
+        [TestCase(3)]
+        [TestCase(2)]
+        [TestCase(1)]
+        public void SloppySearch_LoseSecrecy(int roll)
+        {
+            TestScenario
+                .Given.Game(g => g.Hero())
+                .When.Hero(h => h.DrawsEvent("Sloppy Search"))
+                .When.Player(p => p.SelectsEventOption("Roll", x => x.Rolls(roll)))
+                .When.Player(p => p.SelectsEventOption("Lose Secrecy"))
+                .Then.Hero(h => h.LostSecrecy().Event(e => e.HasOutstanding(0)));
+        }
+
+        [Test]
+        public void SloppySearch_NoGraceToSpend()
+        {
+            TestScenario
+                .Given.Game(g => g.Hero(h=>h.Grace(0)))
+                .When.Hero(h => h.DrawsEvent("Sloppy Search"))
+                .When.Player(p => p.SelectsEventOption("Roll", x => x.Rolls(1)))
+                .Then.Player(p => p.Event(e => e.ActiveRow("Spend 1 Grace or lose 1 Secrecy").HasOptions("Lose Secrecy")));
+        }
+    }
+}
--- Slugburn.DarkestNight.Rules/Events/Cards/SloppySearch.cs
+++ Slugburn.DarkestNight.Rules/Events/Cards/SloppySearch.cs
@@ -1,59 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Slugburn.DarkestNight.Rules.Heroes;
-using Slugburn.DarkestNight.Rules.Rolls;
 
 namespace Slugburn.DarkestNight.Rules.Events.Cards
 {
     public class SloppySearch : IEventCard
     {
-        public EventDetail Detail => EventDetail.Create("Sloppy Search", 0, 
+        public EventDetail Detail => EventDetail.Create("Sloppy Search", 2,
             x => x
-            .Text("Roll 1 die and take the highest")
-            .Row(6, "Gain 1 Secrecy")
-            .Row(4, 5, "No effect")
-            .Row(1, 3, "Spend 1 Grace or lose 1 Secrecy")
-            .Option("roll", "Roll"));
+                .Text("Roll 1d and take the highest")
+                .Row(6, "Gain 1 Secrecy", o => o.Option("gain-secrecy", "Gain Secrecy"))
+                .Row(4, 5, "No effect", o => o.Option("no-effect", "No Effect"))
+                .Row(1, 3, "Spend 1 Grace or lose 1 Secrecy", o => o
+                    .Option("spend-grace", "Spend Grace", h => h.Grace > 0)
+                    .Option("lose-secrecy", "Lose Secrecy"))
+                .Option("roll", "Roll"));
 
         public void Resolve(Hero hero, string option)
         {
-            if (option == "roll")
+            switch (option)
             {
-                hero.RollEventDice(new SloppySearchRollHandler());
-                return;
+                case "roll":
+                    hero.RollEventDice(new EventRollHandler(Detail));
+                    return;
+                case "gain-secrecy":
+                    hero.GainSecrecy(1, int.MaxValue);
+                    break;
+                case "no-effect":
+                    // no effect
+                    break;
+                case "spend-grace":
+                    hero.SpendGrace(1);
+                    break;
+                case "lose-secrecy":
+                    hero.LoseSecrecy(1, "Event");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(option), option);
             }
-            if (option == "gain-secrecy")
-                hero.GainSecrecy(1, int.MaxValue);
-            else if (option == "spend-grace")
-                hero.SpendGrace(1);
-            else if (option == "lose-secrecy")
-                hero.LoseSecrecy(1, "Event");
-            else if (option != "cont")
-                throw new ArgumentOutOfRangeException(nameof(option), option);
-            hero.AcceptRoll();
-        }
-
-        public class SloppySearchRollHandler : IRollHandler
-        {
-            public RollState HandleRoll(Hero hero, RollState rollState)
-            {
-                var e = hero.CurrentEvent;
-                e.Rows.Activate(rollState.Result);
-                if (rollState.Result == 6)
-                    e.Options = new List<HeroEventOption> {new HeroEventOption("gain-secrecy", "Gain Secrecy")};
-                else if (rollState.Result > 3)
-                    e.Options = new List<HeroEventOption> { HeroEventOption.Continue() };
-                else
-                    e.Options = new List<HeroEventOption> { new HeroEventOption("spend-grace", "Spend Grace"), new HeroEventOption("lose-secrecy", "Lose Secrecy") };
-                hero.DisplayCurrentEvent();
-                return rollState;
-            }
-
-            public void AcceptRoll(Hero hero, RollState rollState)
-            {
-                hero.EndEvent();
-            }
+            hero.EndEvent();
         }
     }
 }
